d5508be2ffc22766d2e6000d03f80c3b6e38e863
OluwaseyiOJ6 <richjuve@gmail.com>
2023-04-25 15:45:33 -0700

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-import requests
 
-movie_id = input("Enter the movie ID: ") # prompt user to enter movie ID
-url = f"https://swapi.dev/api/films/{movie_id}/"
-
-# make GET request to API endpoint for movie data
-response = requests.get(url)
-
-if response.status_code == 200:
-    movie_data = response.json()
-    character_urls = movie_data["characters"]
-
-    # loop through each character URL and make GET request to get character data
-    for character_url in character_urls:
-        character_response = requests.get(character_url)
-        if character_response.status_code == 200:
-            character_data = character_response.json()
-            print(character_data["name"])
-else:
-    print(f"Error: Could not retrieve data for movie {movie_id}")
-
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,20 @@
 #!/usr/bin/node
-import requests
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-movie_id = input("Enter the movie ID: ") # prompt user to enter movie ID
-url = f"https://swapi.dev/api/films/{movie_id}/"
-
-# make GET request to API endpoint for movie data
-response = requests.get(url)
-
-if response.status_code == 200:
-    movie_data = response.json()
-    character_urls = movie_data["characters"]
-    characters = []
-
-    # loop through each character URL and make GET request to get character data
-    for character_url in character_urls:
-        character_response = requests.get(character_url)
-        if character_response.status_code == 200:
-            character_data = character_response.json()
-            characters.append(character_data["name"])
-
-    # print character names in order of list "characters" in the /films/ response
-    for character_url in character_urls:
-        for character_name in characters:
-            if character_name in character_url:
-                print(character_name)
-else:
-    print(f"Error: Could not retrieve data for movie {movie_id}")
-
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
